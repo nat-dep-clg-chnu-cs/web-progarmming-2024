6d07cb81133b5ee7e947cd60d19fc06709c60ca1
VVKo <v.v.kovdrysh@gmail.com>
2024-02-08 11:13:21 +0200

    Кінець лекції 2
    Signed-off-by: VVKo <v.v.kovdrysh@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import Hello from "./components/Hello"
+import Header from "./components/Header.jsx";
 
 
 function App() {
+    const msg = "Comment"
+    const showComment = true
+
+    const loading = false
+    const feedbacks = [
+        {
+            id: 1,
+            text: 'Comment 1',
+        },
+        {
+            id: 2,
+            text: 'Comment 2',
+        },
+        {
+            id: 3,
+            text: 'Comment 3',
+        },
+    ]
+
+    const text = 'TEXT456'
+
+    if (loading) return (<div style={{backgroundColor: 'red'}}>Loading...</div>)
 
 
   return (
     <>
-      <Hello />
+        <Header />
+        {showComment && msg }
+
+        <h3>Коментарі: {feedbacks.length}</h3>
+      {/*<Hello />*/}
+        {showComment && (<>
+            <ul>
+                {feedbacks.map((val) => {
+                    return (
+                        <li key={val.id}>{val.id} : {val.text}</li>
+                    )
+                })}
+            </ul>
+        </>)}
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+
+import PropTypes from "prop-types";
+
+const Header = ({text, textColor, bgColor}) => {
+    const MyStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    };
+    return (
+        <header style={MyStyles}>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    );
+};
+
+Header.defaultProps = {
+    text: "Опитувальник UI",
+    bgColor: "rgba(0,0,0,0.4)",
+    textColor: "#ff6095",
+
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+}
+
+
+export default Header;
